07fa223033b8fe03b8cf0f41cc6f72d3088258ec
No-no Groenen <nog312@g.harvard.edu>
2021-11-15 02:44:48 -0500

    windmap prototype and leaf library added

--- web/js/main.js
+++ web/js/main.js
@@ -12,7 +12,9 @@ let promises = [
         row.trip_date = parseDate(row.trip_date)
         row.median_trip_duration_minutes = +parseFloat(row.median_trip_duration_minutes).toFixed(2);
         return row;
-    })
+    }),
+    fetch('https://gbfs.bluebikes.com/gbfs/en/station_information.json')
+        .then(response => response.json())
 ];
 
 Promise.all(promises)
@@ -28,10 +30,14 @@ function createVis(data) {
 
     let tripCountData = data[0];
     let tripDurationData = data[1];
+    let stationData = data[2];
+    console.log("stationdata", stationData)
 
     let cities = ['boston', 'nyc', 'sf']
 
     timeSeriesVis = new TimeSeriesVis('chart-area', tripCountData, cities)
+
+    windMap = new WindMap("wind-map", stationData, [42.356070, -71.086808]);
 }
 
 function updateVisualization() {
