8bb3cbe6fc301c278f6713d845138680b8bee002
No-no Groenen <nog312@g.harvard.edu>
2021-11-21 15:31:30 -0500

    windmap with circles, for all cities

--- web/js/main.js
+++ web/js/main.js
@@ -1,5 +1,24 @@
 let parseDate = d3.timeParse("%Y-%m-%d");
 let parseDateTime = d3.timeParse("%m/%d/%Y %H:%M"); // 3/21/21 19:42
+let parseUTC = d3.utcParse("%Y-%m-%dT%H:%M:%S.%L%Z");
+
+// HTML element ID's for windmap.
+var windIds = {
+    chart: "wind-map",
+    city: "singleCity",
+    getTime: "windTime",
+    setTime: "showWindTime",
+    setDay: "showWindDay"
+}
+
+var stationSources = {
+    boston: 'https://gbfs.bluebikes.com/gbfs/en/station_information.json',
+    sf: 'https://gbfs.baywheels.com/gbfs/en/station_information.json',
+    nyc: 'https://gbfs.citibikenyc.com/gbfs/en/station_information.json'
+}
+
+// Save city selection for windmap, default is set in HTML.
+var singleCity = ''
 
 let metro_labels = {
     sf: 'Bay Area',
@@ -15,17 +34,16 @@ let promises = [
         row.median_trip_duration_minutes = +row.median_trip_duration_minutes
         return row;
     }),
-    fetch('https://gbfs.bluebikes.com/gbfs/en/station_information.json')
-        .then(response => response.json()),
     d3.csv("data/2021-03-21 dayview.csv", row => {
         row.Time = parseDateTime(row.Time)
         row.Riders = +row.Riders
         return row;
     }),
-    d3.csv("data/2017_present_events.csv", row => {
-        row.event_date = parseDate(row.event_date)
+    d3.csv("data/all_trips_by_date.csv", row => {
+        row.trip_start = parseUTC(row.trip_start);
+        row.trip_end = parseUTC(row.trip_end);
         return row;
-    }),
+    })
 ];
 
 Promise.all(promises)
@@ -41,14 +59,19 @@ function createVis(data) {
     let tripData = data[0];
     let stationData = data[1];
     let dayViewData = data[2];
-    let eventData = data[3];
+    let fromToData = data[3];
+
+    console.log("fromToData", fromToData)
 
     let cities = ['sf', 'boston', 'nyc']
 
     timeSeriesVis = new TimeSeriesVis('chart-area', tripData, cities, eventData, 380)
     timeline = new TimeSeriesTimeline("timeSeriesBrush", groupByTripDate(tripData))
-    barVis = new BarVis('aggregateBarChart', tripData, metro_labels, 'Cumulative Trip Count')
-    windMap = new WindMap("wind-map", stationData, [42.356070, -71.086808]);
+
+    let date = new Date("2021-07-01T00:00:00-04:00") // keep -04:00
+    windMap = new WindMap(windIds, fromToData, date);
+    updateWindmap()
+
     dayView = new DayViewRadial('day-view', dayViewData);
 }
 
@@ -69,6 +92,19 @@ function updateVisualization() {
     timeSeriesVis.wrangleData();
     dayView.wrangleData();
     barVis.wrangleData();
+}
+
+function updateWindmap() {
+    const currentCity = document.querySelector(`input[name=${windIds.city}]:checked`).value
+
+    if (currentCity !== singleCity) {
+        singleCity = currentCity
+        fetch(stationSources[currentCity])
+            .then(response => response.json())
+            .then(result => windMap.wrangleData(result.data.stations, currentCity))
+    } else {
+        windMap.filterTime()
+    }
 }
 
 function timeSeriesBrushed() {
