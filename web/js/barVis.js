7f29b04f5a6bf7696e6fe0eba2c0da5fa5f7596b
hewness <gordonhew@gmail.com>
2021-11-21 02:50:41 -0500

    time series updates
    feat: added bar chart
    feat: added comparison table
    feat: added key events
    feat: added hover-over

---
+++ web/js/barVis.js
@@ -0,0 +1,148 @@
+
+/*
+ * BarVis - Object constructor function
+ * @param _parentElement 	-- the HTML element in which to draw the visualization
+ * @param _data						-- the actual data: perDayData
+ */
+
+class BarVis {
+
+    constructor(parentElement, data, category_mappings, xLabel) {
+        this.parentElement = parentElement;
+
+        this.data = data;
+        this.filteredData = this.data;
+        this.category_mappings = category_mappings;
+        this.categories = Object.keys(category_mappings)
+        this.xLabel = xLabel;
+        this.isInit = false;
+
+        this.initVis();
+    }
+
+
+    /*
+     * Initialize visualization (static content, e.g. SVG area or axes)
+     */
+
+    initVis() {
+        let vis = this;
+
+        vis.margin = { top: 30, right: 0, bottom: 20, left: 100 };
+
+        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right,
+            vis.height = 500 - vis.margin.top - vis.margin.bottom;
+
+        // SVG drawing area
+        vis.svg = d3.select("#" + vis.parentElement).append("svg")
+            .attr("width", vis.width + vis.margin.left + vis.margin.right)
+            .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+
+        // Scales and axes
+        vis.x = d3.scaleBand()
+            .rangeRound([0, vis.width])
+            .paddingInner(0.2)
+            .domain(d3.range(0, vis.categories.length));
+
+        vis.y = d3.scaleLinear()
+            .range([vis.height, 0]);
+        vis.xAxis = d3.axisBottom()
+            .scale(vis.x);
+
+        vis.yAxis = d3.axisLeft()
+            .scale(vis.y);
+
+        vis.svg.append("g")
+            .attr("class", "x-axis axis")
+            .attr("transform", "translate(0," + vis.height + ")");
+
+        vis.svg.append("g")
+            .attr("class", "y-axis axis");
+
+        // Axis title
+        vis.svg.append("text")
+            .attr("x", -50)
+            .attr("y", -8)
+            .text(vis.xLabel);
+
+        // (Filter, aggregate, modify data)
+        vis.wrangleData();
+        vis.isInit = true
+    }
+
+    /*
+     * Data wrangling
+     */
+    wrangleData() {
+        let vis = this;
+
+        let cityMap = {}
+        vis.categories.forEach(c => cityMap[c] = 0);
+
+        // Aggregate over priorities, iterate over all data
+        vis.filteredData.forEach(function(day) {
+                cityMap[day.city] += day.trip_count
+            }
+        )
+        vis.displayData = cityMap;
+
+        vis.updateVis();
+    }
+
+    /*
+     * The drawing function
+     */
+
+    updateVis() {
+        let vis = this;
+
+        // keep the scale the same after initialization
+        if (!vis.isInit) {
+            vis.y.domain([0, d3.max(Object.values(vis.displayData))]);
+        }
+
+        let cities = [];
+
+        for (var key in vis.displayData) {
+            cities.push(key);
+        }
+
+        let bars = vis.svg.selectAll(".bar")
+            .data(cities);
+
+        bars.enter().append("rect")
+            .attr("class", d => "bar " + d + "-bar")
+            .merge(bars)
+            // .transition()
+            .attr("width", vis.x.bandwidth())
+            .attr("height", function (d) {
+                return vis.height - vis.y(vis.displayData[d]);
+            })
+            .attr("x", function (d, index) {
+                return vis.x(index);
+            })
+            .attr("y", function (d) {
+                return vis.y(vis.displayData[d]);
+            })
+
+        bars.exit().remove();
+
+        // Call axis function with the new domain
+        vis.svg.select(".y-axis").call(vis.yAxis);
+
+        vis.svg.select(".x-axis").call(vis.xAxis)
+            .selectAll("text")
+            .style("text-anchor", "end")
+            .text(i => vis.category_mappings[cities[i]]);
+    }
+
+    onSelectionChange(selectionStart, selectionEnd) {
+        let vis = this;
+
+        vis.filteredData = vis.data.filter(d =>  d.trip_date >= selectionStart && d.trip_date <= selectionEnd);
+
+        vis.wrangleData();
+    }
+}
--- web/js/timeSeriesVis.js
+++ web/js/timeSeriesVis.js
@@ -1,14 +1,16 @@
+const TIME_SERIES_VIS_DEBUG = false;
+
 class TimeSeriesVis {
 
-
-
-    constructor(parentElement, data, cities) {
+    constructor(parentElement, data, cities, eventData) {
         this.parentElement = parentElement;
         // assumes data is sorted
         this.data = data;
         this.filteredData = data;
         this.formatDate = d3.timeFormat("%Y-%m-%d");
         this.cities = new Set(cities)
+        this.eventData = eventData;
+        this.eventMap = new Map()
         this.initFinished = true;
         this.initVis();
     }
@@ -16,13 +18,13 @@ class TimeSeriesVis {
     initVis() {
         let vis = this;
 
-        console.log(vis.parentElement)
-
-        vis.margin = {top: 120, right: 30, bottom: 20, left: 100};
+        vis.margin = {top: 20, right: 30, bottom: 20, left: 100};
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
 
-        console.log("width:", vis.width, "height:", vis.height)
+        if (TIME_SERIES_VIS_DEBUG) {
+            console.log("width:", vis.width, "height:", vis.height)
+        }
 
         vis.svg = d3.select("#" + vis.parentElement)
             .append("svg")
@@ -59,6 +61,27 @@ class TimeSeriesVis {
             .attr("class", "axis x-axis")
             .attr('transform', 'translate(0,' + vis.height + ')')
 
+        vis.div = d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
+
+        // add the X gridlines
+        vis.xGridLines = vis.svg.append("g")
+            .attr("class", "grid")
+            .attr("transform", "translate(0," + vis.height + ")")
+
+        // add the Y gridlines
+        vis.yGridLines = vis.svg.append("g")
+            .attr("class", "grid")
+
+        // probably a better way to do this
+        vis.eventData.forEach(e => {
+            if (!vis.eventMap.has(e.city)) {
+                vis.eventMap.set(e.city, new Map());
+            }
+            vis.eventMap.get(e.city).set(vis.formatDate(e.event_date), e.description);
+        })
+
         this.wrangleData();
     }
 
@@ -80,10 +103,12 @@ class TimeSeriesVis {
         }
 
         vis.cities = new Set(checkedCities);
-        console.log('cities selected:', vis.cities)
 
         vis.selectedDataType = document.getElementById('timeSeriesDataType').value;
-        console.log('cities selected:', vis.selectedDataType)
+
+        if (TIME_SERIES_VIS_DEBUG) {
+            console.log('cities selected:', vis.selectedDataType)
+        }
 
         if (startDate == undefined && endDate == undefined) {
             vis.filteredData = vis.data.filter(d => vis.cities.has(d.city))
@@ -99,7 +124,9 @@ class TimeSeriesVis {
 
         let cityData = vis.filteredData.filter(d => d.city == city)
 
-        console.log('city:', city, 'data', cityData);
+        if (TIME_SERIES_VIS_DEBUG) {
+            console.log('city:', city, 'data', cityData);
+        }
 
         let cityPathName = city + "Path";
         let cityLineName = city + "Line";
@@ -139,37 +166,63 @@ class TimeSeriesVis {
                 .ease(d3.easeLinear)
                 .attr("stroke-dashoffset", 0);
         }
-
-        // vis[cityPathName].enter()
-        //     .append("path")
-        //     .attr("class", cityLineClass)
-        //     .merge(vis[cityPathName])
-        //     // .transition()
-        //     // improves path animation
-        //     // .attrTween('d', d => d3.interpolatePath(d3.select(this).attr('d'), line(d)))
-        //     // .ease(d3.easeLinear)
-        //     // .duration(10000)
-        //     .attr("d", vis[cityLineName])
-        //     .call(p => {
-        //         p.transition()
-        //             .duration(2000)
-        //             // .attrTween('d', d =stroke> d3.interpolatePath(d3.select(this).attr('d'), line(d)))
-        //             // .attrTween("-dasharray", tweenDash);
-        //
-        //     });
-
         vis[cityPathName].exit().remove();
+
+        // add hover overs
+        let cityPointClass = city + "-point";
+
+        vis[cityPointClass] = vis.svg.selectAll("." + cityPointClass)
+            .data(cityData);
+
+
+        vis[cityPointClass].enter()
+            .append("circle")
+            .attr("class", cityPointClass)
+            .merge(vis[cityPointClass])
+            .attr("r", d => {
+                if (vis.eventMap.has(d.city) && vis.eventMap.get(d.city).has(vis.formatDate(d.trip_date))) {
+                    return 8;
+                }
+                return 1;
+            })
+            .attr("cx", d => vis.x(d.trip_date))
+            .attr("cy", d => vis.y(d[vis.selectedDataType]))
+            .on("mouseover", function(event, d) {
+                vis.div.transition()
+                    .style("opacity", 1);
+
+                let toolTipText =
+                    d.trip_date.toDateString()
+                    + "<br/>"
+                    + "Trips Taken: " + d.trip_count
+                    + "<br/>"
+                    + "Median Trip Duration: " + d.median_trip_duration_minutes.toFixed(2) + " min";
+
+                if (vis.eventMap.has(d.city) && vis.eventMap.get(d.city).has(vis.formatDate(d.trip_date))) {
+                    toolTipText += "<br/><br/>" + vis.eventMap.get(d.city).get(vis.formatDate(d.trip_date))
+                }
+
+                vis.div.html(toolTipText)
+                    .style("left", (event.pageX) + "px")
+                    .style("top", (event.pageY - 50) + "px");
+            })
+            .on("mouseout", function(event, d) {
+                // vis.div.transition()
+                //     .duration(8000)
+                //     .style("opacity", 0);
+            });
+        vis[cityPointClass].exit().remove();
     }
 
     updateVis() {
         let vis = this;
 
-        console.log('Filtered data', vis.filteredData)
+        if (TIME_SERIES_VIS_DEBUG) {
+            console.log('Filtered data', vis.filteredData)
+        }
 
         let trip_metric = vis.filteredData.map(d => d[vis.selectedDataType])
         let trip_metric_max = d3.max(trip_metric);
-
-        console.log('Maximum Trip Count', trip_metric_max);
 
         vis.y.domain([0, trip_metric_max])
 
@@ -179,10 +232,33 @@ class TimeSeriesVis {
             let startDate = vis.filteredData[0].trip_date;
             let endDate = vis.filteredData[vis.filteredData.length - 1].trip_date;
 
-            console.log('Start Date', startDate, 'End Date', endDate);
+            if (TIME_SERIES_VIS_DEBUG) {
+                console.log('Start Date', startDate, 'End Date', endDate);
+            }
 
             vis.x.domain([startDate, endDate])
         }
+
+        //TODO: remove hard coding, compute on the fly
+        let tickValues = [ new Date(2017, 0, 1),
+            new Date(2018, 0, 1),
+            new Date(2019, 0, 1),
+            new Date(2020, 0, 1),
+            new Date(2021, 0, 1)]
+
+        vis.xGridLines
+            .call(d3.axisBottom(vis.x)
+                .tickValues(tickValues)
+                .tickSize(-vis.height)
+                .tickFormat("")
+            );
+
+        vis.yGridLines
+            .call(d3.axisLeft(vis.y)
+                .tickSize(-vis.width)
+                .tickFormat("")
+            );
+
 
         vis.svg.select(".x-axis")
             .transition()
