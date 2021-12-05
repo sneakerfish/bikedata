8dc09958e7ff870271b00ae9852a6ffb88a65c39
No-no Groenen <nog312@g.harvard.edu>
2021-12-05 08:45:48 -0500

    legend

--- web/js/windMap.js
+++ web/js/windMap.js
@@ -21,11 +21,34 @@ class WindMap {
 
 		this.colors = {
 			inactive: "lightgrey",  // no activity.
-			netzero: "#b3995d",    // net zero activity.
-			in: "green",        // more people coming in.
-			out: "#FB4D42",       // more people going out.
-			retour: "#041E42"
+			netzero: "#b3995d",     // net zero activity.
+			in: "green",            // more people coming in.
+			out: "#FB4D42",         // more people going out.
+			retour: "#041E42",
+			legend: "rgba(168,168,168,0.47)",
+			legendfont: "rgba(24,24,24,0.47)"
 		}
+
+		this.circleStyle = {
+			fillOpacity: 0.75,
+			strokeOpacity: 1,
+			strokeWidth: 3,
+		}
+
+		this.legendTitles = {
+			inactive: "Inactive station",
+			netzero: "Net zero movement",
+			in: "More people incoming",
+			out: "More people leaving",
+			retour: "Only round trips"
+		}
+
+		this.legendStyle = {
+			fontSize: 11,
+			show: true
+		}
+
+		this.circleRange = [50, 500]
 
 		// Get default city.
 		this.city = document.querySelector(`input[name=${windIds.city}]:checked`).value
@@ -35,14 +58,6 @@ class WindMap {
 
 	initVis () {
 		let vis = this;
-
-		// Set scale for radius, representing amount of people.
-		vis.r = d3.scaleSqrt()
-			.range([50, 300])
-
-		// Set scale for line-weight.
-		vis.l = d3.scaleLinear()
-			.range([1,10])
 
 		// Set default image location.
 		L.Icon.Default.imagePath = 'img/';
@@ -81,6 +96,8 @@ class WindMap {
 		vis.stationGroup = L.layerGroup().addTo(vis.map);
 		vis.lineGroup = L.layerGroup().addTo(vis.map);
 
+		vis.legend = L.control({position: 'bottomleft'});
+
 		// wrangleData will be called from updateVisualisation.
 	}
 
@@ -91,25 +108,20 @@ class WindMap {
 		vis.city = city
 
 		vis.debug && console.log("stationData", vis.stationData)
-		vis.stationData = vis.stationData.map((d) => {
 
-			// In nyc and sf they use shortname in station_id fields.
-			const station_id = vis.city === 'boston' ? d.station_id : d.short_name
-			return {
-				station_id: station_id,
-				name: d.name,
-				lat: d.lat,
-				lon: d.lon
-			}
-		})
+		// NYC and SF use short_name as station_id in their trip-data.
+		vis.get_station_id = function(d) {
+			return vis.city === 'boston' ? d.station_id : d.short_name
+		}
 
 		// Center the map.
 		vis.mapCenter = vis.centers[vis.city]
 		vis.debug && console.log("singleCity:",vis.city)
 		vis.map.setView(vis.mapCenter, 12)
 
+		// Array to reference trip-data to.
 		vis.stationIds = vis.stationData.map((d) => {
-			return d.station_id
+			return vis.get_station_id(d)
 		})
 
 		vis.debug && console.log("stationIds: ", vis.stationIds)
@@ -148,24 +160,30 @@ class WindMap {
 
 		vis.debug && console.log("time", vis.time)
 
+		// StationInfo has relevant trip-data for this time.
 		vis.stationInfo = {}
 		vis.stationData.forEach((d) => {
-			vis.stationInfo[d.station_id] = {
-				station_id: d.station_id,
+			const station_id = vis.get_station_id(d)
+
+			vis.stationInfo[station_id] = {
+				station_id: station_id,
 				name: d.name,
 				loc: [d.lat, d.lon],
-				in: [],
-				out: [],
-				retour: 0,
-				status: "inactive",
+				links: {
+					in: [],                   // All incoming people [ station_id, station_id ]
+					out: [],                  // all outgoing people [ station_id, station_id ]
+					retour: [],               // array of same station_id's.
+					netzero: [],              // all incoming + outgoing traffic if equal amount.
+					inactive: [],             // always empty
+				},
+				status: "inactive",           // "in", "out", "retour", "netzero"
 				color: vis.colors.inactive,
-				direction: "none",
-				travelers: {
-					in: {},             // in: { station_id: 5 }
+				travellers: {
+					in: {},                   // in: { station_id: 5 }
 					out: {},
-					length: 0,          // number of people either coming, going or both if netzero.
-					min: 0,             // number of people on least connected route.
-					max: 0              // number of people on most connected route.
+					min: 0,                   // number of people on least connected route.
+					max: 0,                   // number of people on most connected route.
+					total: 0,                 // total of people connected.
 				},
 			}
 		})
@@ -179,11 +197,29 @@ class WindMap {
 
 				// Check for return trip.
 				if(d.start_station_id === d.end_station_id){
-					vis.stationInfo[d.start_station_id].retour++
-				}
+					vis.stationInfo[d.start_station_id].links.retour.push(d.start_station_id)
 
-				vis.stationInfo[d.start_station_id].out.push(d.end_station_id)
-				vis.stationInfo[d.end_station_id].in.push(d.start_station_id)
+					vis.stationInfo[d.start_station_id].travellers.total++
+
+					vis.debug && console.log("Round trip happened at station: ", d.start_station_id)
+				} else {
+					vis.stationInfo[d.start_station_id].links.out.push(d.end_station_id)
+					vis.stationInfo[d.end_station_id].links.in.push(d.start_station_id)
+
+					const addTraveler = function(status, station_idA, station_idB) {
+						if (!vis.stationInfo[station_idA].travellers[status][station_idB]) {
+							vis.stationInfo[station_idA].travellers[status][station_idB] = 1
+						} else {
+							vis.stationInfo[station_idA].travellers[status][station_idB]++
+						}
+					}
+
+					addTraveler("out", d.start_station_id, d.end_station_id)
+					addTraveler("in", d.end_station_id, d.start_station_id)
+
+					vis.stationInfo[d.start_station_id].travellers.total++
+					vis.stationInfo[d.end_station_id].travellers.total++
+				}
 
 				vis.currentTrips.push(d)
 			}
@@ -192,64 +228,127 @@ class WindMap {
 		vis.debug && console.log("stationInfo: ", vis.stationInfo)
 		vis.debug && console.log("currentTrips: ", vis.currentTrips)
 
+		// Keep track of what station-status has been set.
+		vis.statusInfo = {
+			in: false,
+			out: false,
+			retour: false,
+			netzero: false
+		}
+
 		vis.displayData = []
 		Object.values(vis.stationInfo).forEach((d) => {
-			d['change'] = -d.out.length + d.in.length
+			const inAmount = d.links.in.length
+			const outAmount = d.links.out.length
+			const retourAmount = d.links.retour.length
 
-			// netzero is same amount of people coming as going.
-			if (d.in.length + d.out.length !== 0 && d.change === 0) {
+			// Netzero is same amount of people coming as going.
+			const inactive = inAmount + outAmount + retourAmount === 0
+			const netzero = inAmount === outAmount && !inactive
+			const incoming = inAmount > outAmount
+			const outgoing = outAmount > inAmount
+			const retour = inAmount === 0 && outAmount === 0 && retourAmount > 0
+
+			if (netzero) {
 				d.status = "netzero"
 				d.color = vis.colors.netzero
-			} else if (d.change > 0) {
-				d.direction = "in"
+				d.links.netzero = [...d.links.in, ...d.links.out]
+				vis.statusInfo.netzero = true
+			} else if (incoming) {
+				d.status = "in"
 				d.color = vis.colors.in
-			} else if (d.change < 0) {
-				d.direction = "out"
+				vis.statusInfo.in = true
+			} else if (outgoing) {
+				d.status = "out"
 				d.color = vis.colors.out
-			} else if (d.change === 0 && d.retour > 0) {
+				vis.statusInfo.out = true
+			} else if (retour) {
 				d.status = "retour"
 				d.color = vis.colors.retour
+				vis.statusInfo.retour = true
 			}
 
-			const dir = d.direction
+			// Set min/max amount of people on route to/from this station.
+			if (incoming || outgoing){
+				const peoplePerStation = Object.values(d.travellers[d.status])
 
-			// Set amount of travelers on route from or to this station.
-			if (dir !== "none") {
-				d[dir].forEach((link) => {
-					if (!d.travelers[dir][link]){
-						d.travelers[dir][link] = 1
-					} else {
-						d.travelers[dir][link]++
-					}
-				})
-
-				// these links will be drawn by lines.
-				d.travelers.links = []
-				Object.entries(d.travelers[dir]).forEach(([key, value]) => {
-					d.travelers.links.push({
-						station_id: key,
-						amount: value
-						}
-					)
-				})
-
-				// amount of people linked to this station.
-				d.travelers.length = d[dir].length
-
-				// set min / max for the line-weight.
-				const people = Object.values(d.travelers[dir])
-				d.travelers.min = d3.min(people)
-				d.travelers.max = d3.max(people)
-
-			} else {
-				// if netzero take total amount.
-				d.travelers.length = d.in.length + d.out.length
+				d.travellers.min = d3.min(peoplePerStation)
+				d.travellers.max = d3.max(peoplePerStation)
 			}
 
 			vis.displayData.push(d)
 		})
 
+		// Render inactive stations first so that they are on the background.
+		const sortOrder = ["inactive", "netzero", "retour", "out", "in"]
+		const sortMethod = function(a, b) {
+			return sortOrder.indexOf(a.status) - sortOrder.indexOf(b.status)
+		}
+
+		vis.displayData = vis.displayData.sort((a,b) => sortMethod(a,b))
+
 		vis.debug && console.log("displayData: ", vis.displayData)
+		// vis.debug && console.log("Statusinfo: ", vis.statusInfo)
+
+		// Least and most people at a station.
+		vis.rMin = d3.min(vis.displayData, d => d.links[d.status].length)
+		vis.rMax = d3.max(vis.displayData, d => d.links[d.status].length)
+
+		// Set scale for radius, representing amount of people. Only for circle area's.
+		vis.r = d3.scaleSqrt()
+			.domain([vis.rMin, vis.rMax])
+			.range(vis.circleRange)
+
+		vis.debug && console.log("rScale: ", vis.rMin, vis.rMax)
+
+		// statusExists = [ "in", "out", "inactive", "retour", "netzero" ]
+		const statusExists = Object.keys(vis.statusInfo).filter((key) => vis.statusInfo[key])
+		vis.debug && console.log("statusExists: ", statusExists)
+
+		// legendData = [ [{status: "in", color: "green", radius:4}, {..}], [ status: "out" ] ]
+		vis.legendData = []
+		statusExists.forEach((status) => {
+
+			const filtered_array = vis.displayData.filter(d => d.status === status)
+
+			const min = d3.min(filtered_array, (d) => d.links[status].length)
+			const max = d3.max(filtered_array, (d) => d.links[status].length)
+
+			// calculate middle point.
+			const scaleCircle = d3.scaleLinear()
+				.domain([0,1])
+				.rangeRound([min, max])
+
+			const mid = scaleCircle(0.5)
+
+			// only create unique circles.
+			const range = [...new Set([min, mid, max])] // [ 2, 3, 3 ] => [ 2, 3 ]
+			// vis.debug && console.log("range: ", range)
+
+			// Create one circle for the legend.
+			const circle = function(amount) {
+				return {
+					status: status,
+					color: vis.colors[status],
+					radius: vis.r(amount),
+					amount: amount
+				}
+			}
+
+			const circleList = []
+			for (let i = 0; i < range.length; i++){
+				circleList.push(
+					circle(range[i])
+				)
+			}
+			// vis.debug && console.log("CircleList", circleList)
+
+			vis.legendData.push(circleList)
+		})
+
+		vis.legendData = vis.legendData.sort((a,b) => a.length - b.length)
+
+		vis.debug && console.log("legendData: ", vis.legendData)
 
 		vis.updateVis();
 	}
@@ -261,18 +360,6 @@ class WindMap {
 		document.getElementById(vis.ids.setDay).innerText = vis.time.toDateString()
 		document.getElementById(vis.ids.setTime).innerText = vis.formatTime(vis.time)
 
-		// Setup radius domain.
-		const rMin = d3.min(vis.displayData, d => d.travelers.length)
-		const rMax = d3.max(vis.displayData, d => d.travelers.length)
-
-		vis.r.domain([rMin, rMax])
-
-		// Setup domain for line-weights.
-		const lMin = d3.min(vis.displayData, d => d.travelers.min)
-		const lMax = d3.max(vis.displayData, d=> d.travelers.max)
-
-		vis.l.domain([lMin, lMax])
-
 		// Clear previous visualization.
 		vis.stationGroup.clearLayers();
 		vis.lineGroup.clearLayers();
@@ -282,28 +369,37 @@ class WindMap {
 			vis.stationGroup.addLayer(circle(d))
 		})
 
+		// Refresh legend when zooming.
+		vis.map.on("zoomend", function() {
+			addLegend()
+		})
+		addLegend()
+
 		function circle (d) {
-			const radius = vis.r(d.travelers.length)
+			const radius = vis.r(d.links[d.status].length)
 
 			return L.circle(d.loc, radius, {
 				color: d.color,
-				fillColor: d.color,
-				strokeWidth: "1px",
+				fillOpacity: vis.circleStyle.fillOpacity,
+				opacity: vis.circleStyle.strokeOpacity,
+				weight: vis.circleStyle.strokeWidth
 				}
 			).on("mouseover", () => addLines(d))
 		}
 
 		function addLines(d) {
-			const { direction, color } = d
+			const { status, color, station_id } = d
 
 			// Remove previous hover line.
 			vis.lineGroup.clearLayers();
 
-			// if this station has a direction,
-			if (direction !== "none"){
-				d.travelers.links.forEach((link) => {
+			// Only add lines if station has net direction.
+			const hasLines = (status === "in" || status === "out")
+
+			if (hasLines){
+				Object.entries(d.travellers[status]).forEach(([link_station, link_amount]) => {
 					vis.lineGroup.addLayer(
-						line(d.station_id, link.station_id, link.amount, color)
+						line(station_id, link_station, link_amount, color)
 					)
 				})
 			}
@@ -312,10 +408,19 @@ class WindMap {
 		function line(idA, idB, amount, color) {
 			const from = vis.stationInfo[idA].loc
 			const to = vis.stationInfo[idB].loc
-			const lineWeight = vis.l(amount)
 			const dot = 8
 			const gap = 2
 			const lineCap = "butt"
+
+			// Setup domain for line-weights.
+			const lMin = d3.min(vis.displayData, d => d.travellers.min)
+			const lMax = d3.max(vis.displayData, d=> d.travellers.max)
+
+			const l = d3.scaleLinear()
+				.domain([lMin, lMax])
+				.range([1, vis.rMax * 2])
+
+			const lineWeight = l(amount)
 
 			return L.polyline([from, to], {
 				color: color,
@@ -325,6 +430,179 @@ class WindMap {
 			})
 		}
 
+		function addLegend() {
+			vis.zoom = vis.map.getZoom()
+			console.log(vis.zoom)
+
+			// Toggle legend on/off else it becomes too big.
+			vis.zoom > 12 && toggleLegend(false)
+			vis.zoom < 13 && toggleLegend(true)
+
+			console.log(vis.zoom)
+			vis.legend.onAdd = setLegend
+			vis.legend.addTo(vis.map)
+		}
+
+		function setLegend() {
+
+			const fontsize = vis.legendStyle.fontSize
+			const fontcolor = vis.colors.legendfont
+
+			const minMeters = vis.r(vis.rMin)
+			const maxMeters = vis.r(vis.rMax)
+
+			// latitude of the center of the city.
+			const latitude = vis.centers[vis.city][0]
+
+			const minPixels = pixelValue(latitude, minMeters, vis.zoom)
+			const maxPixels = pixelValue(latitude, maxMeters, vis.zoom)
+
+			vis.pixelScale = d3.scaleLinear()
+				.domain([minMeters, maxMeters])
+				.range([minPixels, maxPixels])
+
+			// Space a circle takes.
+			const cHeight = maxPixels * 2 + 5
+			const cWidth = maxPixels * 2 + 5
+			const rowHeight = cHeight + fontsize + 5
+
+			// Most circles in a row.
+			const maxLegend = d3.max(vis.legendData, d => d.length)
+
+			// Setup legend size.
+			const width = cWidth * maxLegend + 120;
+			const height = rowHeight * vis.legendData.length + 20;
+			const margin = { left: 15, right: 20, top: 20, bottom: 20 }
+
+			// Setup drawing area.
+			const div = d3.create("div")
+			const svg = div.append("svg")
+				.attr("class", "legend")
+				.attr("display", vis.legendStyle.show ? "block" : "none")
+				.attr("width", width+margin.left+margin.right)
+				.attr("height", height+margin.top+margin.bottom)
+				.append("g")
+				.attr("transform","translate("+[margin.left,margin.top]+")");
+
+			const quad = svg.selectAll("g.rect.legend")
+				.data([0])
+				.enter()
+				.append("rect")
+				.attr("class", "legend")
+				.attr("x", 0)
+				.attr("y", 0)
+				.attr("width", width)
+				.attr("height", height)
+				.attr("fill", vis.colors.legend)
+				.on("click", toggleLegend)
+
+			// Add row for the circles.
+			const rows = svg.selectAll("g.circle-row")
+				.data(vis.legendData, (d) => d.status)
+
+			rows.enter()
+				.append("g")
+				.attr("class", "circle-row")
+				.attr("transform", (d, i) => `translate(0,${i * rowHeight + 10})`)
+
+			rows.enter()
+				.append("text")
+				.attr("class", "legend-title")
+				.attr("x", (d, i) => vis.legendData[i].length * cWidth + fontsize / 2)
+				.attr("y", (d, i) => i * rowHeight + (rowHeight - cHeight) * 0.75 + cHeight +10)
+				.attr("font-size", fontsize)
+				.style("fill", fontcolor)
+				.text((d) => vis.legendTitles[d[0].status])
+				.on("click", toggleLegend)
+
+			// Add the circle.
+			const circles = svg.selectAll("g.circle-row").selectAll("circle.legend-item")
+				.data(d => d)
+
+			circles.enter()
+				.append("circle")
+				.attr("class", "legend-item")
+				.attr("cx", (d, i) => i * cWidth + cWidth / 2)
+				.attr("cy", cHeight / 2)
+				.attr("r", d => vis.pixelScale(d.radius))
+				.attr("fill", d => d.color)
+				.style("fill-opacity", vis.circleStyle.fillOpacity)
+				.style("stroke-opacity", vis.circleStyle.strokeOpacity)
+				.style("stroke-width", vis.circleStyle.strokeWidth)
+				.style("stroke", d => d.color)
+				.on("click", toggleLegend)
+
+			circles.enter()
+				.append("text")
+				.attr("class", "legend-label")
+				.attr("x", (d, i) => i * cWidth + cWidth / 2)
+				.attr("y", (rowHeight - cHeight) * 0.75 + cHeight)
+				.attr("font-size", fontsize)
+				.attr("fill", fontcolor)
+				.attr("text-anchor", "middle")
+				.text(d => d.amount)
+				.on("click", toggleLegend)
+
+			// Setup button to hide the legend.
+			const buttonWidth = 80;
+			const buttonHeight = 30;
+
+			const svgButton = div.append("svg")
+				.attr("class", "button")
+				.attr("display", vis.legendStyle.show ? "none" : "block")
+				.attr("width", buttonWidth+margin.left+margin.right)
+				.attr("height", buttonHeight+margin.top+margin.bottom)
+				.append("g")
+				.attr("transform","translate("+[margin.left,margin.top]+")");
+
+			const button = svgButton.selectAll("g.rect.button")
+				.data([0])
+				.enter()
+				.append("rect")
+				.attr("class", "button")
+				.attr("x", 0)
+				.attr("y", 0)
+				.attr("width", buttonWidth)
+				.attr("height", buttonHeight)
+				.attr("fill", vis.colors.legend)
+				.on("click", toggleLegend)
+
+			svgButton.selectAll("g.text.button-title")
+				.data(["Show"])
+				.enter()
+				.append("text")
+				.attr("class", "button-title")
+				.attr("x", buttonWidth / 2)
+				.attr("y", buttonHeight / 2 + fontsize / 2)
+				.attr("text-anchor", "middle")
+				.attr("fill", fontcolor)
+				.attr("font-size", fontsize)
+				.text(d => d)
+				.on("click", toggleLegend)
+
+			return div.node()
+		}
+
+		// Value is optional, can be true or false
+		function toggleLegend(value) {
+			const isUndefined = typeof value !== 'boolean'
+
+			vis.legendStyle.show = isUndefined ? !vis.legendStyle.show : value
+			const show = vis.legendStyle.show
+			d3.select('svg.legend').attr("display", show ? "block" : "none")
+			d3.select('svg.button').attr("display", show ? "none" : "block")
+		}
+
+		// https://stackoverflow.com/questions/30703212/how-to-convert-radius-in-metres-to-pixels-in-mapbox-leaflet?rq=1
+		function metersPerPixel (latitude, zoomLevel) {
+			const earthCircumference = 40075017;
+			const latitudeRadians = latitude * (Math.PI/180);
+			return earthCircumference * Math.cos(latitudeRadians) / Math.pow(2, zoomLevel + 8);
+		};
+
+		function pixelValue (latitude, meters, zoomLevel) {
+			return meters / metersPerPixel(latitude, zoomLevel);
+		};
 
 	}
 }
